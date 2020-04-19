fa0d292501ffb865115ab3307b32cee2c12ed4fd
Tommy Rachmat <tommy.rachmat@amarbank.co.id>
2020-04-19 22:33:11 +0700

    Initial Commit

---
+++ FantasticView/FantasticView.h
@@ -0,0 +1,19 @@
+//
+//  FantasticView.h
+//  FantasticView
+//
+//  Created by AMARBANK on 19/04/20.
+//  Copyright © 2020 Test. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FantasticView.
+FOUNDATION_EXPORT double FantasticViewVersionNumber;
+
+//! Project version string for FantasticView.
+FOUNDATION_EXPORT const unsigned char FantasticViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FantasticView/PublicHeader.h>
+
+
